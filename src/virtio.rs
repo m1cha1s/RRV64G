177c4901301aad4f056553bd0b5e7c3f80544f1f
m1cha1s <msulej04@gmail.com>
2022-07-28 14:20:48 +0200

    Created virtio block struct

--- src/virtio.rs
+++ src/virtio.rs
@@ -1,4 +1,8 @@
-pub struct VirtioBlock {
+use crate::prelude::MemIntf;
+
+const MAX_BLOCK_QUEUE: u32 = 1;
+
+pub struct VirtioBlock<'a> {
     id: u64,
     driver_features: u32,
     page_size: u32,
@@ -7,5 +11,21 @@ pub struct VirtioBlock {
     queue_pfn: u32,
     queue_notify: u32,
     status: u32,
-    disk: 
+    disk: &'a mut dyn MemIntf,
+}
+
+impl<'a> VirtioBlock<'a> {
+    pub fn new(disk_image: &'a mut dyn MemIntf) -> Self {
+        Self {
+            id: 0,
+            driver_features: 0,
+            page_size: 0,
+            queue_sel: 0,
+            queue_num: 0,
+            queue_pfn: 0,
+            queue_notify: MAX_BLOCK_QUEUE,
+            status: 0,
+            disk: disk_image,
+        }
+    }
 }
